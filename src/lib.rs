201db130df71bdef4a61fe4dd3ee1f16805fdfef
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-01-26 16:10:50 +0000

    Added textures and more

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod buffer;
+mod clock;
 
 use std::thread::sleep;
 use vek::*;
@@ -8,12 +9,19 @@ use minifb::{
     Key,
     MouseMode,
 };
+use image::{
+    self,
+    RgbaImage,
+    DynamicImage,
+};
 use self::buffer::Buffer2d;
+use self::clock::Clock;
 
 #[derive(Copy, Clone)]
 struct Cell {
     height: f32,
     colour: u32,
+    kind: i32,
 }
 
 const WHITE: u32 = 0xFFFFFFFF;
@@ -28,6 +36,7 @@ impl Cell {
         Self {
             height: 0.0,
             colour: WHITE,
+            kind: 0,
         }
     }
 }
@@ -82,23 +91,17 @@ impl World {
         };
 
         for i in 0..20 {
-            let col = if i % 2 == 0 {
-                RED
-            } else {
-                BLUE
-            };
-
             world.cells[0][i].height = 0.7 + (i % 4) as f32 * 0.25;
-            world.cells[0][i].colour = col;
+            world.cells[0][i].kind = 1;
 
             world.cells[20][i].height = 0.7 + (i % 4) as f32 * 0.25;
-            world.cells[20][i].colour = col;
+            world.cells[20][i].kind = 1;
 
             world.cells[i][0].height = 0.7 + (i % 4) as f32 * 0.25;
-            world.cells[i][0].colour = col;
+            world.cells[i][0].kind = 1;
 
             world.cells[i][20].height = 0.7 + (i % 4) as f32 * 0.25;
-            world.cells[i][20].colour = col;
+            world.cells[i][20].kind = 1;
         }
 
         world
@@ -108,9 +111,13 @@ impl World {
         self.cells.get(pos.x).and_then(|col| col.get(pos.y))
     }
 
+    pub fn get_at_mut(&mut self, pos: Vec2<usize>) -> Option<&mut Cell> {
+        self.cells.get_mut(pos.x).and_then(|col| col.get_mut(pos.y))
+    }
+
     pub fn cast_ray<'a>(&'a self, mut pos: Vec2<f32>, dir: Vec2<f32>, mut t: f32, height_thresh: f32) -> Result<(f32, &'a Cell), ()> {
         const PLANCK: f32 = 0.0001;
-        for i in 0..100 {
+        for i in 0..64 {
             let cpos = pos + dir * t;
 
             if let Some(cell) = self.get_at(cpos.map(|e| e as usize)) {
@@ -126,28 +133,61 @@ impl World {
     }
 }
 
+struct Textures {
+    tilemap: RgbaImage,
+    floor: RgbaImage,
+    wall: RgbaImage,
+}
+
+fn rgba_to_u32(p: image::Rgba<u8>) -> u32 {
+    (p.data[0] as u32) << 16 |
+    (p.data[1] as u32) << 8  |
+    (p.data[2] as u32) << 0  |
+    (p.data[3] as u32) << 24
+}
+
+impl Textures {
+    pub fn wall_at(&self, pos: Vec2<f32>) -> u32 {
+        let x = ((128.0 * pos.x) as u32) & 0x7F;
+        let y = ((128.0 * pos.y) as u32) & 0x7F;
+        rgba_to_u32(*self.wall.get_pixel(x, y))
+    }
+
+    pub fn floor_at(&self, pos: Vec2<f32>) -> u32 {
+        let x = ((50.0 * pos.x) as u32) & 0xFF;
+        let y = ((50.0 * pos.y) as u32) & 0xFF;
+        rgba_to_u32(*self.floor.get_pixel(x, y))
+    }
+}
+
 const WIN_SZ: Vec2<usize> = Vec2 { x: 800, y: 600 };
 
 struct Engine {
+    clock: Option<Clock>,
     win: Option<Window>,
     world: Option<World>,
     color: Option<Buffer2d<u32>>,
     keys: Option<Vec<i32>>,
+    tex: Option<Textures>,
     //last_mouse_pos: (u32, u32),
 }
 
 impl Engine {
     const fn new() -> Self {
         Self {
+            clock: None,
             win: None,
             world: None,
             color: None,
             keys: None,
+            tex: None,
             //last_mouse_pos: (0, 0),
         }
     }
 
     fn init(&mut self) {
+        self.clock = Some(Clock::new());
+
         self.win = Some(Window::new(
             "GGJ19",
             WIN_SZ.x,
@@ -162,6 +202,12 @@ impl Engine {
         self.color = Some(Buffer2d::new(WIN_SZ, 0xFFFFFFFF));
 
         self.keys = Some(vec![]);
+
+        self.tex = Some(Textures {
+            tilemap: image::open("assets/floor.png").unwrap().to_rgba(),
+            floor: image::open("assets/floor.png").unwrap().to_rgba(),
+            wall: image::open("assets/wall.jpg").unwrap().to_rgba(),
+        });
     }
 
     fn display(&mut self) {
@@ -174,36 +220,42 @@ impl Engine {
         let mut color = self.color.as_mut().unwrap();
         let mut world = self.world.as_mut().unwrap();
         let mut win = self.win.as_mut().unwrap();
+        let mut tex = self.tex.as_mut().unwrap();
 
         // Render
         color.clear(SKY_BLUE);
 
         for x in 0..WIN_SZ.x {
-            for y in WIN_SZ.y / 2..WIN_SZ.y {
-                color.set(Vec2::new(x, y), BROWN);
-            }
-        }
-
-        for x in 0..WIN_SZ.x {
-            const FOV: f32 = 0.0015;
+            const FOV: f32 = 0.0011;
             let col_dir = world.player
                 .get_look_dir((x as f32 - WIN_SZ.x as f32 / 2.0) * FOV);
 
+            for y in WIN_SZ.y / 2..WIN_SZ.y {
+                let dist = 0.5 * ((y - WIN_SZ.y / 2) as f32 * 0.01).tan().abs();
+
+                let dist = (std::f32::consts::PI / 2.0 - (y as f32 - WIN_SZ.y as f32 / 2.0) * 0.002).tan();
+                color.set(Vec2::new(x, y), tex.floor_at(world.player.pos + col_dir * dist));
+            }
 
             let mut lim_min = WIN_SZ.y;
             let mut height_thresh = 0.0;
             let mut t = 0.0;
-            for i in 0..10 {
+            for i in 0..5 {
                 if let Ok((dist, cell)) = world.cast_ray(world.player.pos, col_dir, t, height_thresh) {
                     let top_height = 1000.0 * (cell.height - 0.5).atan2(dist);
                     let bot_height = 1000.0 * (0.5f32).atan2(dist);
-                    let base = (WIN_SZ.y as f32 / 2.0 - top_height).max(0.0) as usize;
-                    let lim = (WIN_SZ.y / 2 + bot_height as usize).min(WIN_SZ.y);
-                    for y in base..lim.min(lim_min) {
-                        color.set(Vec2::new(x, y), cell.colour);
+                    let base = (WIN_SZ.y as f32 / 2.0 - top_height);
+                    let lim = (WIN_SZ.y / 2 + bot_height as usize);
+
+                    let cpos = world.player.pos + col_dir * dist;
+
+                    for y in base.max(0.0) as usize..lim.min(WIN_SZ.y).min(lim_min) {
+                        let yfract = (y - base as usize) as f32 / (lim - base as usize) as f32;
+                        let pix = tex.wall_at(Vec2::new(cpos.sum().fract(), yfract));
+                        color.set(Vec2::new(x, y), pix);
                     }
                     if (lim < lim_min) {
-                        lim_min = base;
+                        lim_min = base.max(0.0) as usize;
                         height_thresh = cell.height;
                     }
                     t = dist;
@@ -227,12 +279,13 @@ impl Engine {
             }
         }
         self.keys = Some(keys);
+
+        self.clock.as_mut().unwrap().tick(std::time::Duration::from_millis(1000 / 60));
     }
 }
 
 unsafe impl Sync for Engine {}
 
-use spin::Mutex;
 static mut ENGINE: Engine = Engine::new();
 
 #[no_mangle]
@@ -265,4 +318,23 @@ pub extern "C" fn put_camera(x: f32, y: f32, ori: f32) {
     let mut engine = unsafe { &mut ENGINE };
     engine.world.as_mut().unwrap().player.pos = Vec2::new(x, y);
     engine.world.as_mut().unwrap().player.ori = ori;
+}
+
+#[no_mangle]
+pub extern "C" fn set_cell(x: i32, y: i32, height: f32, kind: i32) {
+    let mut engine = unsafe { &mut ENGINE };
+    engine.world.as_mut().unwrap().get_at_mut(Vec2::new(x as usize, y as usize)).map(|cell| cell.height = height);
+    engine.world.as_mut().unwrap().get_at_mut(Vec2::new(x as usize, y as usize)).map(|cell| cell.kind = kind);
+}
+
+#[no_mangle]
+pub extern "C" fn get_cell_height(x: i32, y: i32) -> f32 {
+    let mut engine = unsafe { &mut ENGINE };
+    engine.world.as_mut().unwrap().get_at_mut(Vec2::new(x as usize, y as usize)).map(|cell| cell.height).unwrap_or(0.0)
+}
+
+#[no_mangle]
+pub extern "C" fn get_cell_kind(x: i32, y: i32) -> i32 {
+    let mut engine = unsafe { &mut ENGINE };
+    engine.world.as_mut().unwrap().get_at_mut(Vec2::new(x as usize, y as usize)).map(|cell| cell.kind).unwrap_or(0)
 }
