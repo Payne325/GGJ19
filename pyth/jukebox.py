13eb22896050344804bfe1bb34dcce70947ac121
Payne325 <alex.payne325@gmail.com>
2019-01-27 12:55:45 +0000

    Jukebox is rocking and rolling bby

---
+++ pyth/jukebox.py
@@ -0,0 +1,13 @@
+class Jukebox:
+  def __init__(self, engine):
+    self.music = [3, 8]
+    self.engine = engine
+    self.jbIndex = 0
+
+  def IsPlaying(self):
+    return self.engine.music_is_playing() == 1
+
+  def PlayMusic(self):
+    if self.IsPlaying() == False:
+      self.engine.play_music(self.music[self.jbIndex])
+      self.jbIndex += 1
